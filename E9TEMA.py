454db19fda4cf5df97f016bb37b0d8682045a049
Prog-Josafat <juanmario.mezard@uanl.edu.mx>
2021-05-17 17:19:55 -0500

    Se agrego el metodo consultar todo E9TEMA.py

--- E9TEMA.py
+++ E9TEMA.py
@@ -4,9 +4,15 @@ class TEMA:
         self.nombre=nombre
     
     def guardar (id_tema, nombre):
-        archivo = open ("TEMA.txt", "w", enconding="22f5175")
+        archivo = open ("TEMA.txt", "w", enconding="63da2cf")
         id_tema=input(int("Escribe el id del tema que deasea guardar: /n"))
         nombre=input("Escribe el nombre del tema que desea guardar: /n")
         archivo.write("Este texto se guardara en el archivo: /n")
         archivo.close ()
-
+    
+    def consultartodo(id_tema, nombre):
+        archivo= open("TEMA.txt", enconding="63da2cf")
+        print(archivo.read())
+        archivo.close()
+    
+    
