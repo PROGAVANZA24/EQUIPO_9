ab90cbf4d3e7cf899d82e401525ec702200e3ab6
Prog-Josafat <juanmario.mezard@uanl.edu.mx>
2021-05-17 17:59:10 -0500

    Se actualizo el metodo guradr E9VIDEO.py

--- E9VIDEO.py
+++ E9VIDEO.py
@@ -7,7 +7,9 @@ class Video:
     
     def guardar (id_video, nombre, url, fecha_publicacion):
         archivo = open ("VIDEO.txt", "w", enconding="343ae39")
-        id_tema=input(int("Escribe el id del video que deasea guardar: /n"))
+        id_video=input(int("Escribe el id del video que deasea guardar: /n"))
         nombre=input("Escribe el nombre del video que desea guardar: /n")
         url=input("Escribe el url del video que desea guardar: /n")
-      
+        fecha_publicacion=input(int("Escribe la fecha de publicacion del video que desea guardar(escribirlo con numero): /n"))
+        archivo.write("Este texto se guardara en el archivo: /n")
+        archivo.close ()
