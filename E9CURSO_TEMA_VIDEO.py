c67bd4ce390334884399e6790d3e95c2662464a1
Prog-Josafat <juanmario.mezard@uanl.edu.mx>
2021-05-17 18:28:19 -0500

    Se agrego el metodo guardar E9CURSO_TEMA_VIDEO.py

--- E9CURSO_TEMA_VIDEO.py
+++ E9CURSO_TEMA_VIDEO.py
@@ -4,4 +4,9 @@ class Curso_Tema_Video:
         self.id_CT=id_CT
         self.id_video=id_video
     
-    
+    def guardar (id_CTV, id_CT, id_video):
+        archivo = open ("VIDEO.txt", "w", enconding="343ae39")
+        id_CTV=input(int("Escribe el id del curso que deasea guardar: /n"))
+        id_CT=input(int("Escribe el id del tema que deasea guardar: /n"))
+        id_video=input(int("Escribe el id del video que deasea guardar: /n"))
+        
