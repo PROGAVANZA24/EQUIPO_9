544daead875b4fb6f8ed0f8ca88711762de78015
Prog-Josafat <juanmario.mezard@uanl.edu.mx>
2021-05-17 17:47:06 -0500

    Se agrego la clase Video E9VIDEO.py

---
+++ E9VIDEO.py
@@ -0,0 +1,7 @@
+class Video:
+    def __init__(self, id_video, nombre, url, fecha_publicacion):
+        self.id_video=id_video
+        self.nombre=nombre
+        self.url=url
+        self.fecha_publicacion=fecha_publicacion
+        
