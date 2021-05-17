43fb4f9c9ce8e0f4c5549cfa37dd5e9fc26eca6b
Prog-Josafat <juanmario.mezard@uanl.edu.mx>
2021-05-17 18:33:23 -0500

    Se actualizo el metodo guardar y se agrego el metodo consultar todo E9CURSO_TEMA_VIDEO.py

--- E9CURSO_TEMA_VIDEO.py
+++ E9CURSO_TEMA_VIDEO.py
@@ -5,8 +5,14 @@ class Curso_Tema_Video:
         self.id_video=id_video
     
     def guardar (id_CTV, id_CT, id_video):
-        archivo = open ("VIDEO.txt", "w", enconding="343ae39")
+        archivo = open ("CURSO_TEMA_VIDEO.txt", "w", enconding="ea4102e")
         id_CTV=input(int("Escribe el id del curso que deasea guardar: /n"))
         id_CT=input(int("Escribe el id del tema que deasea guardar: /n"))
         id_video=input(int("Escribe el id del video que deasea guardar: /n"))
-        
+        archivo.write("Este texto se guardara en el archivo /n")
+        archivo.close ()
+    
+    def consultartodo(id_CTV, id_CT, id_video):
+        archivo= open("CURSO_TEMA_VIDEO.txt", enconding="ea4102e")
+        print(archivo.read())
+        archivo.close()
