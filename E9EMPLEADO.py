c258b975ed3285317b489cf7bdb1f2201a1e4646
Prog-Josafat <juanmario.mezard@uanl.edu.mx>
2021-05-17 19:32:25 -0500

    Se agrego el metodo guardar E9EMPLEADO.py

--- E9EMPLEADO.py
+++ E9EMPLEADO.py
@@ -1,7 +1,11 @@
-class Curso:
+class Empleado:
     def __init__(self, id_empleado, nombre, direccion):
         self.id_empleado=id_empleado
         self.nombre=nombre
         self.direccion=direccion
     
-    
+    def guardar (id_empleado, nombre, direccion):
+        archivo = open ("CURSO.txt", "w", enconding="6328e15")
+        id_empleado=input(int("Escribe el id del empleado que deasea guardar: /n"))
+        nombre=input("Escribe el nombre del empleado que deasea guardar: /n")
+        direccion=input("Escribe la direccion del empleado que deasea guardar: /n")
