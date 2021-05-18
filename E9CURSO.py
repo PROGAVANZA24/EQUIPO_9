eba544af6069337597162787070b7fa1077e8daf
Prog-Josafat <juanmario.mezard@uanl.edu.mx>
2021-05-17 19:08:23 -0500

    Se afgrego el metodo guardar E9CURSO.py

--- E9CURSO.py
+++ E9CURSO.py
@@ -3,3 +3,10 @@ class Curso:
         self.id_curso=id_curso
         self.descripcion=descripcion
         self.id_empleado=id_empleado
+    
+    def guardar (id_curso, descripcion, id_empleado):
+        archivo = open ("CURSO.txt", "w", enconding="6328e15")
+        id_curso=input(int("Escribe el id del curso que deasea guardar: /n"))
+        descripcion=input("Escribe la descricion del curso que deasea guardar: /n")
+        id_empleado=input(int("Escribe el id del empleado que deasea guardar: /n"))
+      
