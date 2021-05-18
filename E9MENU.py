910c91a68477a1cf5ca2c4015d93e1f4a288d821
csoto0505 <soarc93@gmail.com>
2021-05-18 11:34:21 -0500

    se agregaron las primeras lineas de codigo del menu E9MENU.py

--- E9MENU.py
+++ E9MENU.py
@@ -4,3 +4,8 @@ from E9CURSO_TEMA_VIDEO import *
 from E9CURSO_TEMA import *
 from E9CURSO import *
 from E9EMPLEADO import *
+print("Bienvenido a nuestra aplicación que es lo que desea guardar /n")
+print("Almacenar datos de un Tema [1] /nm")
+print("Almacenar datos de un Video [2] /n")
+print("Almacenar datos de un Curso, Tema, Video [3] /n")
+print("Almacenar datos de un Curso, Tema [4] /n")
