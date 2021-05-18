44b40f1f51d65a0b13cc5007a86cb35d634cd748
ProgsMauricio <mauricio.garciasldn@uanl.edu.mx>
2021-05-17 21:46:29 -0500

    se agrego el metodo guardar MAUE9CT.py

--- MAUE9CT.py
+++ MAUE9CT.py
@@ -3,3 +3,8 @@ class Curso_Tema:
         self.id_CT=id_CT
         self.id_curso=id_curso
         self.id_tema=id_tema
+    def guardar (id_CT, id_curso, id_tema):
+        archivo = open ("CURSO_TEMA.txt", "w", enconding="ea4102e")
+        id_CT=input(int("Escribe el id del curso del tema que deasea guardar: /n"))
+        id_curso=input(int("Escribe el id del curso que deasea guardar: /n"))
+        id_tema=input(int("Escribe el id del tema que deasea guardar: /n"))
